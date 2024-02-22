982c3e0b3664f28d41fa7483f7eab0dcf50cb29e
Maham Farooq <67932027+CoderMF@users.noreply.github.com>
2024-02-22 10:01:25 -0500

    Update streamlit_app.py
    used pandas to pull data into a dataframe called my_fruit_list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit as sl
+import pandas as pd
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+sl.df(my_fruit_list)
 
 sl.title('My Parents New Healthy Diner')
 
