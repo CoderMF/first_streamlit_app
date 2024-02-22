be8bdf88055c5affffe9f60d5d54d6ddfda4c877
Maham Farooq <67932027+CoderMF@users.noreply.github.com>
2024-02-22 09:56:54 -0500

    Update streamlit_app.py
    added text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as sl
 
 sl.title('My Parents New Healthy Diner')
-sl.header('Breakfast Menu')
-sl.text('Omega 3 & blueberry Oatmeal')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
